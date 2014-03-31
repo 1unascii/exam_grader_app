c63ed305d5e97b807b40bc75d1760fd8c348a602
MATC Student <student@MATC1234.(none)>
2014-03-31 18:49:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExamGraderApp::Application.config.secret_key_base = '3d1afc0d1ce1f0af0626ff46e7dfc738191903b105febdef4990e270036e00ea942a6adbffb05579bdd1e86897fe35a8f56b77de5c8c06ec49f6122943234db8'
